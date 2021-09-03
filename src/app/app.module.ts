57cf50b1390327b4c215435d027707485a9afbb5
Pooja14206 <pooja.14206@gmail.com>
2021-09-03 10:33:03 +0530

    added home page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,23 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './component/header/header.component';
+import { HomeComponent } from './component/home/home.component';
+import { AddComponent } from './component/add/add.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    HomeComponent,
+    AddComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/model/person.ts
@@ -0,0 +1,9 @@
+export class Person {
+    id!: number;
+    name!: String;
+    city!: String; 
+    state!: String;
+    emailId!: String;
+    zip!: Number;
+    phonneNumber!: Number;
+}
